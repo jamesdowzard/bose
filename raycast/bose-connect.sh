c48a9a563d7296680efc52c7741adaeb227e2a26
jamesdowzard <james.dowzard@gmail.com>
2026-06-14 12:47:04 +1000

    feat: Apple TV in the Raycast connect dropdown

--- raycast/bose-connect.sh
+++ raycast/bose-connect.sh
@@ -7,6 +7,6 @@
 # @raycast.mode compact
 # @raycast.icon 🎧
 # @raycast.packageName Bose
-# @raycast.argument1 { "type": "dropdown", "placeholder": "Device", "data": [{"title":"Mac","value":"mac"},{"title":"Phone","value":"phone"},{"title":"iPad","value":"ipad"},{"title":"iPhone","value":"iphone"},{"title":"Quest","value":"quest"},{"title":"TV","value":"tv"}] }
+# @raycast.argument1 { "type": "dropdown", "placeholder": "Device", "data": [{"title":"Mac","value":"mac"},{"title":"Phone","value":"phone"},{"title":"iPad","value":"ipad"},{"title":"iPhone","value":"iphone"},{"title":"Quest","value":"quest"},{"title":"TV","value":"tv"},{"title":"Apple TV","value":"appletv"}] }
 
 "$HOME/bin/bose" connect "$1"
